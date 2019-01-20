cbfcd456507fbb1cf81197a0bb05f87907b4d2a6
Austin Schuetz <ausi427@gmail.com>
2019-01-20 12:07:23 -0700

    ssrapp functions from nuxt-firebase-pwa, npm start at the end of build

--- ssrfunctions.js
+++ ssrfunctions.js
@@ -1,16 +1,24 @@
 const functions = require('firebase-functions');
-const { Nuxt } = require('nuxt');
 const express = require('express');
+const { Nuxt } = require('nuxt');
 
 const app = express();
+const nuxt = new Nuxt({
+    dev: false,
+    buildDir: 'public',
+    build: {
+        publicPath: '/public/'
+    }
+});
 
-const config = {
-    dev: false
-};
-const nuxt = new Nuxt(config);
+function handleRequest(req, res) {
+    res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
+    return new Promise((resolve, reject) => {
+        nuxt.render(req, res, (promise) => {
+            promise.then(resolve).catch(reject);
+        });
+    });
+}
 
-app.use(nuxt.nuxtssr);
-
-exports.nuxtssr = functions.https.onRequest(app);
-
-response.end()
+app.use(handleRequest);
+exports.ssrapp = functions.https.onRequest(app);
