8a8bfa4dc822527ea4d2fa49113207edc4423a6e
Austin Schuetz <ausi427@gmail.com>
2019-02-21 16:31:02 -0700

    prerender testing functions app1?

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,9 @@ app.use(require('prerender-node').set('prerenderToken', 'NyLo86U3Oey8sbHZlSG9'))
 app.get("*", (request, response) => {
     response.send("Hello from Express on Firebase!")
 })
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const app1 = functions.https.onRequest(app)
+
+module.exports = {
+    app1
+}
