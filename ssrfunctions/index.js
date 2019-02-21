761c7d0144be65f8c26ab9df8edc4f90d54c34ae
Austin Schuetz <ausi427@gmail.com>
2019-02-21 12:33:18 -0700

    removing app1 function

--- ssrfunctions/index.js
+++ ssrfunctions/index.js
@@ -3,13 +3,13 @@ const express = require('express');
 app.use(require('prerender-node').set('prerenderToken', 'NyLo86U3Oey8sbHZlSG9'));
 
 /* Express */
-const app = express()
-app.get("*", (request, response) => {
-    response.send("Hello from Express on Firebase!")
-})
-
-const app1 = functions.https.onRequest(app)
-
-module.exports = {
-    app1
-}
+// const app = express()
+// app.get("*", (request, response) => {
+//     response.send("Hello from Express on Firebase!")
+// })
+//
+// const app1 = functions.https.onRequest(app)
+//
+// module.exports = {
+//     app1
+// }
