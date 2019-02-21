d8f4f93f6bf3b873b11f097cb59679df78bdfc9c
Austin Schuetz <ausi427@gmail.com>
2019-02-21 11:43:10 -0700

    manual firebase admin update to help functions folder

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,18 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const express = require('express');
+app.use(require('prerender-node').set('prerenderToken', 'NyLo86U3Oey8sbHZlSG9'));
+
+/* Express */
+const app = express()
+app.get("*", (request, response) => {
+    response.send("Hello from Express on Firebase!")
+})
+
+const app1 = functions.https.onRequest(app)
+
+module.exports = {
+    app1
+}
