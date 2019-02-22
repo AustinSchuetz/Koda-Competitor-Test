b4beb0558d03c11a132e0ccf0fb6d27332a76f1e
Austin Schuetz <ausi427@gmail.com>
2019-02-22 12:10:52 -0700

    retry with firebase init basics

--- functions/index.js
+++ functions/index.js
@@ -6,18 +6,3 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
-const express = require('express');
-
-/* Express */
-const app = express()
-app.use(require('prerender-node').set('prerenderToken', 'NyLo86U3Oey8sbHZlSG9'));
-app.get("*", (request, response) => {
-    response.send("Hello from Express on Firebase!")
-})
-
-const app1 = functions.https.onRequest(app)
-
-module.exports = {
-    app1
-}
